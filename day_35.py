33f19b843b7f52987845505b7884df90dff45182
ranbeerbhangu9-crypto <ranbeerbhangu9@gmail.com>
2026-03-02 09:44:52 +0530

    initial commit

---
+++ day_35.py
@@ -0,0 +1,11 @@
+#reverse a number using while loop
+
+n = int(input("Enter a number: "))
+rev = 0
+
+while n > 0:
+    dig = n % 10
+    rev = rev * 10 + dig
+    n = n // 10
+
+print(f"Reversed number is: {rev}")
