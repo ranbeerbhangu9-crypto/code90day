5db2c86fb2f9cff0198235a0ec7a8f94646ee424
ranbeerbhangu9-crypto <ranbeerbhangu9@gmail.com>
2026-03-19 09:13:23 +0530

    initial commit

---
+++ day_52.py
@@ -0,0 +1,10 @@
+#indexing value & Slicing
+
+t = ("apple", "banana", "cherry","Peach","Kiwi","Grapes")
+
+print("The tuple is:-\n",t)
+print(f"First index: {t[0]}")
+print(f"Last index: {t[-1]}")
+print("For all index:-",t[0:7])
+print("for special middle value:-",t[1:4])
+
