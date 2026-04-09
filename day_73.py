1da3eccd14b474e7785ba232527e1f245f99b3e6
ranbeerbhangu9-crypto <ranbeerbhangu9@gmail.com>
2026-04-09 20:48:25 +0530

    initial commit

---
+++ day_73.py
@@ -0,0 +1,11 @@
+#Merging two dictionary
+
+d1 = {"N1":"Prince","A1":19}
+d2 = {"N2":"Ranveer","A2":20}
+
+print("first dictionary:",d1)
+print("Second dictionary:",d2)
+
+d1.update(d2)
+
+print("After merging:",d1)
