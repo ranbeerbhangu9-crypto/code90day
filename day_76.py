b43f92be2238228e8b2342c95f1e6bdfd51e663b
ranbeerbhangu9-crypto <ranbeerbhangu9@gmail.com>
2026-04-12 20:47:23 +0530

    initial commit

---
+++ day_76.py
@@ -0,0 +1,11 @@
+#User input dictionary
+d = {}
+
+n = int(input("Enter number of items: "))
+
+for i in range(n):
+    key = input("Enter key: ")
+    value = input("Enter value: ")
+    d[key] = value
+
+print("Dictionar:",d)
